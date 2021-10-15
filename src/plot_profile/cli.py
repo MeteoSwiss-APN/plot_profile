ea4cd3325ad5fb489bc3aebdf3f5c4479b65c104
Michel Zeller <mizeller@student.ethz.ch>
2021-10-15 15:09:42 +0200

    commit-friday-3

--- src/plot_profile/cli.py
+++ src/plot_profile/cli.py
@@ -7,18 +7,19 @@ Date: 05/10/2021.
 # Standard library
 from typing import NamedTuple
 
+# Third-party
+import click
+
 # Local
-from .functions import *
+from .get_data import *
+from .plot_data import *
 
 
 @click.command()
 # @click.argument("station_id") # make the station ID non-optional
 @click.option("--station_id", default="06610", help="station ID: XXXXX - def: 06610")
 @click.option(
-    "--start", default="2021083100", help="start date: YYYYMMDDHH - def: 2021083100"
-)
-@click.option(
-    "--end", default="2021083100", help="end date: YYYYMMDDHH - def: 2021083100"
+    "--date", default="2021083100", help="start date: YYYYMMDDHH - def: 2021083100"
 )
 @click.option(
     "--alt_bot",
@@ -38,8 +39,6 @@ from .functions import *
         [
             "743",
             "winddir",
-            "744",
-            "press",
             "745",
             "temp",
             "747",
@@ -50,11 +49,11 @@ from .functions import *
         case_sensitive=False,
     ),
     multiple=True,
-    default=("743", "745", "748", "747", "744"),
+    default=("743", "745", "748", "747"),
     help="Default: all",
 )
 @click.option(
-    "--out_path",
+    "--outpath",
     default="plots/",
     type=str,
     help="path to folder where the plots should be saved - def: plots/",
@@ -67,6 +66,7 @@ from .functions import *
 @click.option(
     "--clouds",
     is_flag=True,
+    # default=True,
     help="Show clouds on plot - def: True",
 )
 @click.option(
@@ -75,89 +75,88 @@ from .functions import *
     type=float,
     help="Define the relative humidity threshold for clouds - def: 80",
 )
+@click.option(
+    "--print_steps",
+    is_flag=True,
+    help="Add this flag to display intermediate steps.",
+)
+@click.option(
+    "--standard_settings",
+    is_flag=True,
+    help="temp_range: -100-30 [°C], windvel_range: 0-50 [km/h]",
+)
+@click.option(
+    "--personal_settings",
+    is_flag=True,
+    help="If this flag is added, personal 'standard' settings can be defined using the temp_min/max and windvel_min/max flags",
+)
+@click.option(
+    "--temp_min",
+    type=float,
+    help="Define the minimum temperature. Disclaimer: Add --personal_settings flag!",
+)
+@click.option(
+    "--temp_max",
+    type=float,
+    help="Define the maximum temperature. Disclaimer: Add --personal_settings flag!",
+)
+@click.option(
+    "--windvel_min",
+    type=float,
+    help="Define the minimum windvelocity. Disclaimer: Add --personal_settings flag!",
+)
+@click.option(
+    "--windvel_max",
+    type=float,
+    help="Define the maximum windvelocity. Disclaimer: Add --personal_settings flag!",
+)
 def main(
     *,
     station_id: str,
-    start: str,
-    end: str,
+    date: str,
     params: tuple,
     alt_bot: int,
     alt_top: int,
-    out_path: str,
+    outpath: str,
     grid: bool,
     clouds: bool,
     relhum_thresh: float,
+    print_steps: bool,
+    standard_settings: bool,
+    personal_settings: bool,
+    temp_min: float,
+    temp_max: float,
+    windvel_min: float,
+    windvel_max: float,
 ) -> None:
-    # choose if intermediate steps should be printed (for mere debugging purposes)
-    print_steps = False
 
-    # this is the standard station_type we try to consider
-    station_type = "profile"
-
-    # station_id and parameter_id dicts
-    params_dict = {
-        "743": "743",
-        "winddir": "743",
-        "744": "744",
-        "press": "744",
-        "745": "745",
-        "temp": "745",
-        "746": "746",
-        "relhum": "746",
-        "747": "747",
-        "dewp": "747",
-        "748": "748",
-        "windvel": "748",
-    }
-    stations_dict = {"06610": "PAYERNE"}
-
-    start, end, params, params_tuple, station_name = reformat_inputs(
-        start=start,
-        end=end,
+    df, station_name, relevant_params = get_data(
+        date=date,
         params=params,
         station_id=station_id,
-        params_dict=params_dict,
-        stations_dict=stations_dict,
-        print_steps=print_steps,
-    )
-
-    # create dataframes
-    df = dwh2pandas(
-        params=params,
-        start=start,
-        end=end,
-        station_id=station_id,
-        print_steps=print_steps,
-    )
-
-    station_height = df["elev"].iloc[0]
-    if station_height > alt_bot:
-        alt_bot = station_height
-
-    if alt_top == 40000:
-        alt_top = df["742"].max() * 1.05
-
-    params_df = extract_columns(
-        params_tuple=params_tuple,
-        data=df,
         print_steps=print_steps,
         alt_bot=alt_bot,
         alt_top=alt_top,
     )
 
-    # # saving the dataframe as csv
-    # params_df.to_csv('params_df.csv')
-    # df.to_csv('df.csv')
-
     create_plot(
-        df=params_df,
+        df=df,
         relhum_thresh=relhum_thresh,
         grid=grid,
         clouds=clouds,
-        outpath=out_path,
+        outpath=outpath,
         station_name=station_name,
-        start=start,
+        date=date,
         alt_top=alt_top,
         alt_bot=alt_bot,
-        params=params,
+        params=relevant_params,
+        print_steps=print_steps,
+        standard_settings=standard_settings,
+        personal_settings=personal_settings,
+        temp_min=temp_min,
+        temp_max=temp_max,
+        windvel_min=windvel_min,
+        windvel_max=windvel_max,
     )
+
+    print("--- Done.")
