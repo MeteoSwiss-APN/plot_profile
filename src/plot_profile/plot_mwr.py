57b4422f4ccf40b482cca649c959696eabb82f4f
Stephanie Westerhuis <stephanie.westerhuis@usys.ethz.ch>
2022-01-31 10:34:44 +0100

    consistent filenames

--- src/plot_profile/plot_mwr.py
+++ src/plot_profile/plot_mwr.py
@@ -57,13 +57,12 @@ def mwr_heatmap(
     plt.tight_layout()
 
     # save figure
-    # filename = f'mwr_heatmap_{date.strftime("%y%m%d")}_{date.hour:02}_{var.short_name}_{station.short_name}'
-    filename = f"mwr_heatmap_{var.short_name}_{station.short_name}"
+    name = f'heatmap_mwr_{date.strftime("%y%m%d_%H")}_{station.short_name}_{var.short_name}'
     if appendix:
-        filename = filename + "_" + appendix
+        name = name + "_" + appendix
 
     save_fig(
-        filename=filename,
+        filename=name,
         datatypes=[
             "png",
         ],
