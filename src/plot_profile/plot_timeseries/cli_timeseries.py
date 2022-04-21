bcc01b35ecbdefeaaf78ac9cc1e2397bedc7997c
Stephanie Westerhuis <stephanie.westerhuis@usys.ethz.ch>
2022-04-21 17:21:27 +0200

    timeseries examples

--- src/plot_profile/plot_timeseries/cli_timeseries.py
+++ src/plot_profile/plot_timeseries/cli_timeseries.py
@@ -175,7 +175,7 @@ def main(
 
     Example commands:
     plot_timeseries --loc pay --start 21111900 --end 21111912 --add_obs 2m ver_vis --add_obs 2m cbh
-    plot_timeseries --loc pay --start 21111900 --end 21111906 --add_model icon temp 1 ref --add_model icon temp 10 exp --add_model icon temp 2 ref --add_model icon temp 20 exp --add_obs 2m temp --add_obs 2m_tower temp --add_obs 2m dewp_temp --add_model icon 2m_temp 0 ref --model_src ref /scratch/swester/output_icon/ICON-1/ 21111812 --model_src exp /scratch/swester/output_icon/exp1/ 21111812
+    plot_timeseries --loc pay --start 21111900 --end 21111906 --add_model icon temp 1 ref --add_model icon temp 1 exp --add_obs 10m_tower temp --model_src ref /scratch/swester/output_icon/ICON-1/ 21111812 --model_src exp /scratch/swester/output_icon/exp1/ 21111812
     old way:
     plot_timeseries --loc gla --start 21111900 --end 21111902 --device 5cm --device 2m --var temp
     """
