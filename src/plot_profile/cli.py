121bd55ed2b2cecff781fe474ab2c1d7db30577f
Stephanie Westerhuis <stephanie.westerhuis@usys.ethz.ch>
2022-10-11 16:17:15 +0200

    print available stations and variables

--- src/plot_profile/cli.py
+++ src/plot_profile/cli.py
@@ -3,14 +3,40 @@
 # Third-party
 import click
 
+# First-party
+from plot_profile.utils.stations import sdf
+from plot_profile.utils.variables import vdf
+
 # Local
 from . import __version__
 
+# from ipdb import set_trace
 
-def print_version(ctx, value: bool) -> None:
+
+def print_version(ctx, param, value: bool) -> None:
     """Print the version number and exit."""
     if value:
         click.echo(__version__)
+        ctx.exit(0)
+
+
+def print_variables(ctx, param, value: bool) -> None:
+    """Print the available variable names and exit."""
+    if value:
+        print("The following variables are available:")
+        # loop over variables (columns)
+        for var in vdf:
+            print(f"{vdf[var].long_name} -> {vdf[var].short_name}")
+        ctx.exit(0)
+
+
+def print_stations(ctx, param, value: bool) -> None:
+    """Print the available variable names and exit."""
+    if value:
+        print("The following stations are available:")
+        # loop over stations (columns)
+        for station in sdf:
+            print(f"{sdf[station].long_name} -> {sdf[station].short_name}")
         ctx.exit(0)
 
 
@@ -24,6 +50,22 @@ def print_version(ctx, value: bool) -> None:
     is_flag=True,
     expose_value=False,
     callback=print_version,
+)
+@click.option(
+    "--stations",
+    "-S",
+    help="Print available stations and exit.",
+    is_flag=True,
+    expose_value=False,
+    callback=print_stations,
+)
+@click.option(
+    "--variables",
+    "-N",
+    help="Print available variable names and exit.",
+    is_flag=True,
+    expose_value=False,
+    callback=print_variables,
 )
 def main() -> None:
     """Plot observations and ICON variabels from NWP forecasts.
