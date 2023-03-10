85e5796dd0745fd6036565e640392750141568df
David Leutwyler <david.leutwyler@meteoswiss.ch>
2023-03-10 20:12:47 +0100

    remove accidentially checked in code

--- src/plot_profile/plot_icon/get_icon.py
+++ src/plot_profile/plot_icon/get_icon.py
@@ -283,9 +283,9 @@ def get_icon(
         # specify variable (pandas dataframe with attributes)
         var = vdf[variable]
 
-#        print([i for i in ds.data_vars])
         # find correct icon name from list of possible names
         var.icon_name = get_icon_name(ds, var, verbose)
+        
         # subselect values from column
         try:
             values = ds.isel(cells_1=ind)[var.icon_name].values * var.mult + var.plus
